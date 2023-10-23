d5c91cdbeec7b5fa336a17d6334766db518c025b
dappnodedev <diego@dappnode.io>
2023-10-23 13:46:33 +0200

    Fix security paths

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -3,23 +3,23 @@
 case "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_HOLESKY" in
 "prysm-holesky.dnp.dappnode.eth")
   echo "Using prysm-holesky.dnp.dappnode.eth"
-  JWT_PATH="/security/prysm-holesky/jwtsecret.hex"
+  JWT_PATH="/security/prysm/jwtsecret.hex"
   ;;
 "lighthouse-holesky.dnp.dappnode.eth")
   echo "Using lighthouse-holesky.dnp.dappnode.eth"
-  JWT_PATH="/security/lighthouse-holesky/jwtsecret.hex"
+  JWT_PATH="/security/lighthouse/jwtsecret.hex"
   ;;
 "lodestar-holesky.dnp.dappnode.eth")
   echo "Using lodestar-holesky.dnp.dappnode.eth"
-  JWT_PATH="/security/lodestar-holesky/jwtsecret.hex"
+  JWT_PATH="/security/lodestar/jwtsecret.hex"
   ;;
 "teku-holesky.dnp.dappnode.eth")
   echo "Using teku-holesky.dnp.dappnode.eth"
-  JWT_PATH="/security/teku-holesky/jwtsecret.hex"
+  JWT_PATH="/security/teku/jwtsecret.hex"
   ;;
 "nimbus-holesky.dnp.dappnode.eth")
   echo "Using nimbus-holesky.dnp.dappnode.eth"
-  JWT_PATH="/security/nimbus-holesky/jwtsecret.hex"
+  JWT_PATH="/security/nimbus/jwtsecret.hex"
   ;;
 *)
   echo "Using default"
